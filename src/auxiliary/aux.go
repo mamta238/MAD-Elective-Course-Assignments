de17d1543d1cb539256ddcb8fe8edf3c9d774cb0
mamta238 <41248612+mamta238@users.noreply.github.com>
2019-01-28 23:43:10 +0530

    Invokes methods of reader/writer/filter interface. Added comments after last commit

--- src/auxiliary/aux.go
+++ src/auxiliary/aux.go
@@ -9,10 +9,11 @@ import (
 		dbrepo "dbrepository"
 		)
 		
+//Variables for command line interface 
 		
 func Init() (string,string,string,string){
 
-	dbname := "Restaurant"
+	dbname := "Ass1"
 	prompt := "->"
 	guidelines := "Type '-o' for options\n"
 	options := "1) find     --type_of_food/--name/--id/--pcode \n2) list     -To display all Restaurants \n3) store    -To Insert new restaurant record \n4) delete   --id \n5) count    --type_of_food/--pcode \n6) -o        -For Options \n7)Search \n8) -q       Quit"
@@ -22,6 +23,7 @@ func Init() (string,string,string,string){
 }
 		
 		
+//For displaying results of various commands
 
 func DisplayRec(res *domain.Restaurant){
 		
@@ -38,6 +40,9 @@ func DisplayRec(res *domain.Restaurant){
 		
 	}
 
+
+//For 'list' command to list all restaurants:Invokes GetAll from dbrepo 
+
 func ListAllRestaurants (repoAccess *dbrepo.MongoRepository) {
 	
 	res , err := repoAccess.GetAll()
@@ -50,43 +55,50 @@ func ListAllRestaurants (repoAccess *dbrepo.MongoRepository) {
 	}
 }
 	
+	
+//For 'find' command : Gives results according to option and value entered 	
+//Invokes FindByTypeOfFood(),FindByTypeOfPostCode(),FindByName() and Get() methods from dbrepo
+
 func FindAccordingToOption(repoAccess *dbrepo.MongoRepository,option string,value string) {
 		
-	switch strings.Trim(option,"-") {
 		
-		case "type_of_food" : 	res , err := repoAccess.FindByTypeOfFood(value)
+		
+		
+	switch  option {
+		
+		case "--type_of_food" : 	res , err := repoAccess.FindByTypeOfFood(value)
 								fmt.Println(res,err)		
 								if err!=nil {
-								  		fmt.Println("yes",err)
+								  		fmt.Println(err)
 								  	} else {
 								  		for i:=0 ; i<len(res) ; i++{
 								  		DisplayRec(res[i])
 								  	}		
 								}
 								
-			case "pcode"	: 	res , err := repoAccess.FindByTypeOfPostCode(value)
+			case "--pcode"	: 	res , err := repoAccess.FindByTypeOfPostCode(value)
 								if err!=nil {
-								  		fmt.Println("yes",err)
+								  		fmt.Println(err)
 								  	} else {
 								 		for i:=0 ; i<len(res) ; i++{
 								  		DisplayRec(res[i])
 								  	}
 								}
 									
-			case "name"		:	 res , err := repoAccess.FindByName(value)
+			case "--name"		:	 res , err := repoAccess.FindByName(value)
 								 if err!=nil {
-								  		fmt.Println("yes",err)
+								  		fmt.Println(err)
 								  	} else{
 								  		for i:=0 ; i<len(res) ; i++{
 								  		DisplayRec(res[i])
 								  	}
 								  }
 								  
-			case "id"		: 
+			case "--id"		: 
 								 if (domain.IsValidID(value)) {				//if valid id entered
 									res , err := repoAccess.Get(domain.StringToID(value))
 								  	if err!=nil{
-								  		fmt.Println("yes",err)
+								  		fmt.Println(err)
 								  	} else {
 								  		DisplayRec(res)
 								  	}
@@ -98,19 +110,26 @@ func FindAccordingToOption(repoAccess *dbrepo.MongoRepository,option string,valu
 		}
 	}
 	
+
+//For 'count' command : Gives results according to option and value entered 	
+//Invokes FindByTypeOfFood(),FindByTypeOfPostCode() methods from dbrepo and prints count of restaurants matching value
+
 	
 func CountAccordingToOption(repoAccess *dbrepo.MongoRepository,option string,value string) {
 		
-		switch strings.Trim(option,"-") {
+		switch option {
 		
-			case "type_of_food" :  res ,_ := repoAccess.FindByTypeOfFood(value)
+			case "--type_of_food" :  res ,_ := repoAccess.FindByTypeOfFood(value)
 								   fmt.Println(len(res))	
 			
-			case "pcode"		:  res ,_ := repoAccess.FindByTypeOfPostCode(value)
+			case "--pcode"		:  res ,_ := repoAccess.FindByTypeOfPostCode(value)
 								   fmt.Println(len(res))	
+								   
+			default  		:   fmt.Println("Invalid option for count")					   
 		}
 }
 
+//Helper function to read from stdin
 	
 func readValue() string {
 		
@@ -118,6 +137,8 @@ func readValue() string {
 	value, _ := reader.ReadString('\n')
 	return strings.Trim(value,"\n")
 }	
+	
+//For 'store' command :For inserting a new record : Invokes Store() method
 	
 func StoreRecord(repoAccess *dbrepo.MongoRepository) {
 	
@@ -153,6 +174,8 @@ func StoreRecord(repoAccess *dbrepo.MongoRepository) {
 		
 }
 	
+//For 'delete' command: Invokes Delete() method from dbrepo	
+	
 func DeleteRecord(repoAccess *dbrepo.MongoRepository,id string) {
 	
 	if (domain.IsValidID(id)) {
@@ -168,6 +191,7 @@ func DeleteRecord(repoAccess *dbrepo.MongoRepository,id string) {
 		}	
 }
 	
+//For search command -> searching according to a keyword:Invokes Search method from dbrepo
 	
 func SearchOnKeyWord(repoAccess *dbrepo.MongoRepository, query string){
 		
