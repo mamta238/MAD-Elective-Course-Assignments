bc74d83cf3ec2d80efe1900de52c1225c325b885
mamta238 <41248612+mamta238@users.noreply.github.com>
2019-02-12 01:36:08 +0530

    Added DTO for user update

---
+++ Assignment2/delivery/restapplication/usercrudhandler/reqdto.go
@@ -0,0 +1,15 @@
+package usercrudhandler
+
+type UserCreateReqDTO struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Age       int    `json:"age"`
+}
+
+type UserUpdateDTO struct {
+	ID		  string `json:"id"`		
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Age       int    `json:"age"`
+}
+
