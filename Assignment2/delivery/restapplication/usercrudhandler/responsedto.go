1b74ab54de7e13a91ebb63b3a215f879a4647fbd
mamta238 <41248612+mamta238@users.noreply.github.com>
2019-02-12 01:41:54 +0530

    Added UserUpdateRespDTO

---
+++ Assignment2/delivery/restapplication/usercrudhandler/responsedto.go
@@ -0,0 +1,26 @@
+package usercrudhandler
+
+type UserGetRespDTO struct {
+	ID        string `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Age       int    `json:"age"`
+	CreatedOn uint64 `json:"createdOn"`
+}
+
+type UserGetListRespDTO struct {
+	Users []UserGetRespDTO `json:"users"`
+	Count int              `json:"count"`
+}
+
+type UserCreateRespDTO struct {
+	ID string `json:"id"`
+}
+
+type UserUpdateRespDTO struct {
+	ID        string `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Age       int    `json:"age"`
+	CreatedOn uint64 `json:"createdOn"`
+}
